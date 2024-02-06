0f5cdda84576f31c5cfa0084bd0460063fb4181d
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2024-02-06 21:33:16 +0200

    set and get script

--- 0x03-queuing_system_in_js/1-redis-op.js
+++ 0x03-queuing_system_in_js/1-redis-op.js
@@ -12,3 +12,20 @@ client.on('connect', () => {
 client.on('error', (error) => {
     console.error(`Redis client not connected to the server: ${error}`);
 });
+
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, (error, reply) => {
+        redis.print(`Reply: ${reply}`);
+    });
+}
+
+function displaySchoolValue(schoolName) {
+    client.get(schoolName, (error, reply) => {
+        console.log(reply);
+    })
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
